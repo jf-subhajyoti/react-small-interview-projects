7f007beccbac93dd2b8e5f3574690575029bedef
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2024-01-23 23:30:33 +0530

    Added previous and next btn

--- pagination/src/component/paggination/Paggination.jsx
+++ pagination/src/component/paggination/Paggination.jsx
@@ -2,25 +2,27 @@ import React from 'react';
 
 import './Paggination.css';
 
-const Paggination = ({ selectedPage = 0, setPage=() => {}, totalPage = 0}) => {
-    console.log(totalPage, '////////////////////')
+const Paggination = ({ selectedPage = 1, setPage = () => { }, totalPage = 0 }) => {
     const handlePageChange = (pageNo) => {
+        if(pageNo < 1 || pageNo > totalPage) return;
         if (selectedPage === pageNo) return;
         else setPage(pageNo)
     }
-  return (
-    <div className='page_wrapper'>
-      {
-        Array.from({length: totalPage})?.map((ele, index) => {
-            return (
-                <div className={`${index + 1 === selectedPage ? 'page_btn selected' : 'page_btn' }`} onClick={() => {
-                    handlePageChange(index+1)
-                }} key={index}>{index+1}</div>
-            )
-        })
-      }
-    </div>
-  )
+    return (
+        <div className='page_wrapper'>
+            <div className={`${selectedPage === 1 ? 'page_btn disabled' : 'page_btn'}`} onClick={() => handlePageChange(selectedPage-1)}>⏮️</div>
+            {
+                Array.from({ length: totalPage })?.map((ele, index) => {
+                    return (
+                        <div className={`${index + 1 === selectedPage ? 'page_btn selected' : 'page_btn'}`} onClick={() => {
+                            handlePageChange(index + 1)
+                        }} key={index}>{index + 1}</div>
+                    )
+                })
+            }
+            <div className={`${selectedPage === totalPage ? 'page_btn disabled' : 'page_btn'}`} onClick={() => handlePageChange(selectedPage + 1)}>⏭️</div>
+        </div>
+    )
 }
 
 export default Paggination
