3b94b74cd886cd2a08ba1c52a96958ac84a54ad6
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2024-01-29 22:47:13 +0530

    Users added

---
+++ multi-select/src/core/users.js
@@ -0,0 +1,122 @@
+export const users = [
+    {
+        "fullname": "Terry Medhurst",
+        "id": 1
+    },
+    {
+        "fullname": "Sheldon Quigley",
+        "id": 2
+    },
+    {
+        "fullname": "Terrill Hills",
+        "id": 3
+    },
+    {
+        "fullname": "Miles Cummerata",
+        "id": 4
+    },
+    {
+        "fullname": "Mavis Schultz",
+        "id": 5
+    },
+    {
+        "fullname": "Alison Reichert",
+        "id": 6
+    },
+    {
+        "fullname": "Oleta Abbott",
+        "id": 7
+    },
+    {
+        "fullname": "Ewell Mueller",
+        "id": 8
+    },
+    {
+        "fullname": "Demetrius Corkery",
+        "id": 9
+    },
+    {
+        "fullname": "Eleanora Price",
+        "id": 10
+    },
+    {
+        "fullname": "Marcel Jones",
+        "id": 11
+    },
+    {
+        "fullname": "Assunta Rath",
+        "id": 12
+    },
+    {
+        "fullname": "Trace Douglas",
+        "id": 13
+    },
+    {
+        "fullname": "Enoch Lynch",
+        "id": 14
+    },
+    {
+        "fullname": "Jeanne Halvorson",
+        "id": 15
+    },
+    {
+        "fullname": "Trycia Fadel",
+        "id": 16
+    },
+    {
+        "fullname": "Bradford Prohaska",
+        "id": 17
+    },
+    {
+        "fullname": "Arely Skiles",
+        "id": 18
+    },
+    {
+        "fullname": "Gust Purdy",
+        "id": 19
+    },
+    {
+        "fullname": "Lenna Renner",
+        "id": 20
+    },
+    {
+        "fullname": "Doyle Ernser",
+        "id": 21
+    },
+    {
+        "fullname": "Tressa Weber",
+        "id": 22
+    },
+    {
+        "fullname": "Felicity O'Reilly",
+        "id": 23
+    },
+    {
+        "fullname": "Jocelyn Schuster",
+        "id": 24
+    },
+    {
+        "fullname": "Edwina Ernser",
+        "id": 25
+    },
+    {
+        "fullname": "Griffin Braun",
+        "id": 26
+    },
+    {
+        "fullname": "Piper Schowalter",
+        "id": 27
+    },
+    {
+        "fullname": "Kody Terry",
+        "id": 28
+    },
+    {
+        "fullname": "Macy Greenfelder",
+        "id": 29
+    },
+    {
+        "fullname": "Maurine Stracke",
+        "id": 30
+    }
+]
